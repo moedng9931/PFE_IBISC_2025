035a0210f631961e319af06ffe16b05bd0b9555e
medng009 <m.nouga99@proton.me>
2025-04-02 14:49:21 +0200

    Update train_resnet.py

--- train_resnet.py
+++ train_resnet.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyperparamètres
 batch_size = 64
 learning_rate = 0.001
-num_epochs = 10
+num_epochs = 30
 
 # Transformations et augmentation des données
 transform = transforms.Compose([
